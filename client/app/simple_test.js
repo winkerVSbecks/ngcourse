536a2adaca9591e5464bc3d79f54d606bc154cae
Nick Van Weerdenburg <nick@rangle.io>
2015-01-27 14:04:37 -0500

    intial simple test

--- client/app/simple_test.js
+++ client/app/simple_test.js
@@ -1,15 +1,15 @@
-var expect = chai.expect;
+// var expect = chai.expect;
 
-describe('tasks', function () {
-  it('2*2 should equal 4', function () {
-    var x = 2 * 2;
-    var y = 4;
-    // Assert that x is defined.
-    expect(x).to.not.be.undefined;
-    // Assert that x equals to specific value.
-    expect(x).to.equal(4);
-    // Assert that x equals to y.
-    expect(x).to.equal(y);
-    // See http://chaijs.com/api/bdd/ for more assertion options.
+// Define a test suite.
+  describe('tasks', function () {
+    // Define a test.
+    it('should have 2*2 be equal to 4', function () {
+      var x;
+      // Do something.
+      x = 2 * 2;
+      // Check that the results are what we expect and throw an error if something is off.
+      if (x!==4) {
+        throw new Error('Failure of basic arithmetics.');
+      }
+    });
   });
-});
