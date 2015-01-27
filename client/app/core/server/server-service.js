bcf4e86e13a8fe86ce50ef8da3a4161f0309d5b3
Nick Van Weerdenburg <nick@rangle.io>
2015-01-27 16:00:50 -0500

    base for starting testing working session

--- client/app/core/server/server-service.js
+++ client/app/core/server/server-service.js
@@ -1,16 +1,28 @@
 angular.module('ngcourse.server', [])
 
-.factory('server', function ($http) {
-	var service = {};
+.constant('API_BASE_URL', 'http://ngcourse.herokuapp.com')
 
-    var baseUrl = 'http://ngcourse.herokuapp.com';
+.factory('server', function ($http, API_BASE_URL) {
+  var service = {};
 
-    service.get = function (path) {
-      return $http.get(baseUrl + path)
-        .then(function(response) {
-          return response.data;
-        });
-    };
+  service.get = function (path) {
+    return $http.get(API_BASE_URL + path)
+    .then(function (response) {
+      return response.data;
+    });
+  };
 
-    return service;
+  service.post = function (path, data) {
+    return $http.post(API_BASE_URL + path, data);
+  }
+
+  service.put = function (path, id, data) {
+    return $http.put(API_BASE_URL + path + '/' + id, data);
+  }
+
+  service.delete = function (path, id) {
+    return $http.delete(API_BASE_URL + path + '/' + id, data);
+  }
+
+  return service;
 });
