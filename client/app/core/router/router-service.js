68c672239c6d78dc56e1dbf554a7e9729402094c
Nick Van Weerdenburg <nick@rangle.io>
2015-01-28 10:24:00 -0500

    initial ui-router config

--- client/app/core/router/router-service.js
+++ client/app/core/router/router-service.js
@@ -14,5 +14,13 @@
       .state('tasks', {
         url: '/tasks',
         template: 'my tasks'
-      });
+      })
+    .state('tasksDetail', {
+      url: '/tasks/details',
+      template: 'task details'
+    })
+    .state('account', {
+      url: '/my-account',
+      template: 'my account'
+    });      
   });
