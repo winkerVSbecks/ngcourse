9a11fb97022a58d686225aacbc8ef53592571bbb
Nick Van Weerdenburg <nick@rangle.io>
2015-01-30 09:24:17 -0500

    final example

--- client/app/sections/profile/profile-edit-controller.js
+++ client/app/sections/profile/profile-edit-controller.js
@@ -4,17 +4,8 @@ angular.module('ngcourse')
 
 .controller('ProfileEditCtrl', function ($log, users, $stateParams, router, koast) {
   var vm = this;
-
-  console.log("ProfileEditCtrl $stateParams");
-  console.log($stateParams);
-  console.log("profile:");
-  console.log(koast.user.data);
   
   vm.user = users.getUserByUsername(koast.user.data.username);
-
-  console.log("user:");
-  console.log(vm.user);
-
 
   vm.cancel = router.goToTaskList;
 
