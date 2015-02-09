29a9378f5ca3ebcb42bbf59426a4c2fff5b7176b
Seth Davenport <seth.davenport.art@gmail.com>
2015-02-09 12:17:02 -0600

    Updates up to end of day 1.

--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -2,6 +2,37 @@
 
 angular.module('ngcourse')
 
-.controller('TaskListCtrl', function () {
+.controller('TaskListCtrl', function($log, $http, $q) {
+  var vm = this;
 
+  vm.numberOfTasks = 0;
+  vm.tasks = [];
+  vm.sortColumnName = 'owner';
+  vm.sortColumnReversed = false;
+
+  vm.setSortColumn = function(columnName) {
+    if (columnName === vm.sortColumnName) {
+      vm.sortColumnReversed = !vm.sortColumnReversed;
+    }
+    else {
+      vm.sortColumnName = columnName;
+      vm.sortColumnReversed = false;
+    }
+  };
+
+  $http.get('http://ngcourse.herokuapp.com/api/v1/tasks')
+    .then(function(response) {
+      $log.info(response);
+      return response.data;
+    })
+    .then(function(tasksArray) {
+      vm.tasks = tasksArray;
+    })
+    .then(null, function(error) {
+      $log.error(error);
+    });
+
+  vm.addTask = function() {
+    vm.numberOfTasks += 1;
+  };
 });
