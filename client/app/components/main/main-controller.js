e1558fdbd1ab957bf2d1059cb070c3e2ddf13230
Yuri Takhteyev <yuri@yto.io>
2015-04-22 13:06:26 -0400

    End of the controller section.

--- client/app/components/main/main-controller.js
+++ client/app/components/main/main-controller.js
@@ -2,6 +2,19 @@
 
 angular.module('ngcourse')
 
-.controller('MainCtrl', function () {
-
+.controller('MainCtrl', function MainCtrl($log) {
+  var vm = this;
+  vm.username = 'Cosmin';
+  vm.isAuthenticated = true;
+  // vm.isAuthenticated = true;
+  vm.login = function(username, password) {
+    if (password === 'x') {
+      vm.isAuthenticated = true;
+      vm.username = username;
+    } else {
+      $log.info('Wrong password.');
+    }
+  }
 });
+
+
--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -2,6 +2,28 @@
 
 angular.module('ngcourse')
 
-.controller('TaskListCtrl', function () {
-
+.controller('TaskListCtrl', function TaskListCtrl($log) {
+  var vm = this;
+  vm.numberOfTasks = 42;
+  vm.tasks = [
+    {
+      owner: 'alice',
+      description: 'Build the dog shed.'
+    },
+    {
+      owner: 'bob',
+      description: 'Get the milk.'
+    },
+    {
+      owner: 'alice',
+      description: 'Fix the door handle.'
+    }
+  ];
+  vm.addTask = function addTask() {
+    // if (vm.username === 'Cosmin') {
+    //   vm.username = 'Yuri';
+    //   vm.numberOfTasks += 10;      
+    // } else {
+    vm.numberOfTasks += 1;
+  };
 });
